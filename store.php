639fc91a70588aeb7639f46d5ea97d0a67c71f65
Nluo923 <38440217+Nluo923@users.noreply.github.com>
2022-07-18 19:02:13 -0700

    store frontend w/placeholder images

---
+++ store.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=stylesheet href=styles/store.css>
+    <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E>
+    <link rel=" preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <title>SneakerSeeker | Home</title>
+</head>
+
+<body>
+    <ul class=nav-bar>
+        <li aria-current=page class=nav-home><a href=store.php>Home</a></li>
+        <li><a href=profile.php>Profile</a></li>
+        <li><a href=cart.php>Cart</a></li>
+    </ul>
+
+    <div class=store>
+        <div class=store-brand>
+            <div class=store-label>
+                <h2>The Best by Brand</h2>
+            </div>
+            <div class=store-scroll-menu>
+                <div class=store-container>
+                    <img src=img/nike.png>
+                    <div class=container-overlay>
+                        <p>Nike</p>
+                    </div>
+                </div>
+                <div class=store-container>
+                    <img src=img/nike.png>
+                    <div class=container-overlay>
+                        <p>Nike</p>
+                    </div>
+                </div>
+                <div class=store-container>
+                    <img src=img/nike.png>
+                    <div class=container-overlay>
+                        <p>Nike</p>
+                    </div>
+                </div>
+                <div class=store-container>
+                    <img src=img/nike.png>
+                    <div class=container-overlay>
+                        <p>Nike</p>
+                    </div>
+                </div>
+                <div class=store-container>
+                    <img src=img/nike.png>
+                    <div class=container-overlay>
+                        <p>Nike</p>
+                    </div>
+                </div>
+                <div class=store-container>
+                    <img src=img/nike.png>
+                    <div class=container-overlay>
+                        <p>Nike</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
