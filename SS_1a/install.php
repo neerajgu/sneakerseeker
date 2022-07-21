fcf869c04f615c0b751ab0dd26cb6748b18727fc
Nluo923 <38440217+Nluo923@users.noreply.github.com>
2022-07-21 16:17:04 -0700

    sdsd

--- SS_1a/install.php
+++ SS_1a/install.php
@@ -13,12 +13,19 @@
             $query = file_get_contents('database/store.sql');
             $dbh->exec($query);
             echo "<p>Successfully installed databases</p>";
+            $pass = password_hash("password", PASSWORD_DEFAULT);
+            
+            $admin = $dbh->prepare("INSERT INTO `users` (`username`, `password`, `admin`) VALUES (\"admin\", :pass, true)");
+            $admin->bindValue(":pass", $pass);
+            $admin->execute();
+
+            echo "<p>Created admin account   username: admin password: password</p>";
             //https://www.php.net/manual/en/function.file-get-contents.php
             //https://www.php.net/manual/en/pdo.exec
         }
         catch (PDOException $e) {
             echo "<p>Error: {$e->getMessage()}</p>";
         }
-        ?> 
+        ?>
     </body>
 </html>
