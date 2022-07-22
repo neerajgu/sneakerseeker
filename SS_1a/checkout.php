3051e59e7767622e745d7779cd6153b7b776484f
Ruddhra <81442624+Ruddhra@users.noreply.github.com>
2022-07-22 11:53:12 -0700

    work in progress
    I am not done yet

---
+++ SS_1a/checkout.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title></title>
+   </head>
+   <body>
+      
+   </body>
+</html>
